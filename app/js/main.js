329f9491b507f4faf1d9b13751bfa456bd738872
Alexandr <buralex89@gmail.com>
2017-04-13 00:31:34 +0300

    second

--- app/js/main.js
+++ app/js/main.js
@@ -15,41 +15,22 @@ function nextButton(button, form) {
 
  	function forward() {
 
- 		var activeLi = document.querySelectorAll('.test-questions li.active');
- 		//console.log(activeLi);
- 		var xx = document.querySelectorAll('.test-questions li.active input');
- 		debugger;
+ 		var radiosVisible = document.querySelectorAll('.test-questions li.active input');
 
-		for (var i = 0; i <= list.length; i++) {
+ 		if (radiosVisible[0].checked == false && radiosVisible[1].checked == false ) {alert('Выберите ответ!');}
 
-					// if nothing selected
-					// getting inputs which are visible
-				var idRad = "input[id^='q-"+(current+1)+"-";
-				
-				
-				var radiosVisible = document.querySelectorAll(idRad);
-				
+		if (radiosVisible[0].checked || radiosVisible[1].checked ) {
 
-				if (radiosVisible[0].checked == false && radiosVisible[1].checked == false ) {alert('ddd'); break;}
-				
+			var activeLi = document.querySelector('.test-questions li.active');
+ 			activeLi.classList.remove('active');
 
-				if (radiosVisible[0].checked || radiosVisible[1].checked ) {
+ 			var nextLi = activeLi.nextElementSibling;
+ 			nextLi.classList.add('active');
+ 			current++;
+		}
 
-		       		if (list[i].classList.contains('active')) {
-						list[i].classList.remove('active');
-						current = i + 1;
-						//break;
-					}
-
-
-					list[current].classList.add('active');
-
-					if (current == (list.length - 1)) {
-						document.querySelector(button).classList.add('hidden');
-					}
-
-		    	} 
-				
+		if (current == (list.length - 1)) {
+			document.querySelector(button).classList.add('hidden');
 		}
 
 	}
@@ -62,7 +43,6 @@ function nextButton(button, form) {
 		if (key == 13) {
 			//forward();
 			e.preventDefault();
-			
 		}
 	}
 }
